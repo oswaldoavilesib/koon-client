24daa907aa4cdacae36027098c956c845fb83716
Oswaldo <oswaldoavilesib@gmail.com>
2022-03-13 08:19:36 -0600

    Creating context for user auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,8 @@ import type { AppProps } from "next/app";
 import { lightTheme } from "../themes";
 import { ThemeProvider } from "@mui/material";
 import { SWRConfig } from "swr";
-import { UiProvider,CartProvider } from "../context";
+import { UiProvider, CartProvider, AuthProvider } from "../context";
 import "../styles/globals.css";
-
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,13 +13,15 @@ function MyApp({ Component, pageProps }: AppProps) {
           fetch(resource, init).then((res) => res.json()),
       }}
     >
-      <CartProvider>
-        <UiProvider>
-          <ThemeProvider theme={lightTheme}>
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </UiProvider>
-      </CartProvider>
+      <AuthProvider>
+        <CartProvider>
+          <UiProvider>
+            <ThemeProvider theme={lightTheme}>
+              <Component {...pageProps} />
+            </ThemeProvider>
+          </UiProvider>
+        </CartProvider>
+      </AuthProvider>
     </SWRConfig>
   );
 }
