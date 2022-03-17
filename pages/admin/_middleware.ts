69c0d54a8b7f0162cc160f45fcbb2679b8db7c62
Oswaldo <oswaldoavilesib@gmail.com>
2022-03-17 07:26:09 -0600

    Working in local but not in deploy

--- pages/admin/_middleware.ts
+++ pages/admin/_middleware.ts
@@ -9,13 +9,13 @@ export async function middleware( req: NextRequest | any, ev: NextFetchEvent ) {
 
     if ( !session ) {
         const requestedPage = req.page.name;
-        return NextResponse.redirect(`https://koonshop.herokuapp.com//auth/login?p=${requestedPage}`);
+        return NextResponse.redirect(`http://localhost:3000/auth/login?p=${requestedPage}`);
     }
 
     const validRoles = ['admin','super-user','SEO'];
 
     if ( !validRoles.includes( session.user.role ) ) {
-        return NextResponse.redirect('https://koonshop.herokuapp.com//');
+        return NextResponse.redirect('http://localhost:3000/');
     }
 
 
